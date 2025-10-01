8a234b9ed9e862d48009218bc625e8a901fc608b
ArrayOfLilly <arrayoflilly@gmail.com>
2025-10-01 14:24:48 +0200

    add user_prompt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     try:
@@ -14,15 +16,36 @@ def main():
         raise (f"An error occurred while retrieving GEMINI_API_KEY: {e}")
 
     client = genai.Client(api_key=api_key)
-
+    
+    verbose = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--verbose" or sys.argv[1] == "-h":
+            print_usage()
+        user_prompt = sys.argv[1]
+        if len(sys.argv) > 2:
+            verbose = sys.argv[2] == "--verbose"
+    else:
+        print_usage()
+        
+    messages = [
+        types.Content(role="user",parts=[types.Part(text=user_prompt)]),]
+    
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        model='gemini-2.0-flash-001',
+        contents=messages
+        )
+    
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     print("Response:")
-    print(response.text)    
+    print(response.text)
+    
+def print_usage():
+    print("Usage: uv run main.py \"Your prompt here\") [--verbose]")
+    os._exit(1)
 
 if __name__ == "__main__":
     main()
